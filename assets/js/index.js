ba85b4efeb461f61629be097939c8bf1ae1839e0
Innocent Rangaka <itrangaka@gmail.com>
2024-02-19 14:05:08 +0200

    moving img folder to assets folder

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ const portfolioItems = document.querySelectorAll('.portfolio > .portfolio__item'
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
     navLinks.forEach(link => {
-        // console.log(link)
+        // console.log(link);
     });
 });
 
@@ -15,23 +15,20 @@ navLinks.forEach(link => {
     })
 });
 
-
 // Looping through Portfolio Items
 portfolioItems.forEach(item => {
     // Get the image element and extracting Image Information
     let itemImage = item.querySelector("img");
     let itemSrc = itemImage.src;
-
-    // Processing Filename and creating Title:
+     // Processing Filename and creating Title:
     // separates the URL path into an array.
     // gets the last element (filename).
     // gets the filename before the .extension.
     // replaces hyphens and underscores with spaces.
     // created element with classes for styling and appended to the item.
-    let fName = itemSrc.split("/").pop().split(".")[0]; 
+    let fName = itemSrc.split("/").pop().split(".")[0];
     let name = fName.replace("-", " ").replace("_", " ");
-
-     // Create a title element and append it to the item
+    // Create a title element and append it to the item
     let createSpan = document.createElement("span");
     createSpan.classList.add("portfolio__title");
     createSpan.classList.add("text-shadow-dark");
@@ -42,8 +39,8 @@ portfolioItems.forEach(item => {
     // Set href based on filename content:
     // converts the filename to lowercase for conditional checks.
     // Modifying Links Based on Filename.
-    let fNameToLowerCase = fName.toLocaleLowerCase(); 
-    
+    let fNameToLowerCase = fName.toLocaleLowerCase();
+
     if(fNameToLowerCase.includes("sdf")){
         item.href = `${item.href}?iframe=${item.getAttribute("data-view-path")};fname=${itemSrc.split("/").pop()}`;
     }
@@ -76,7 +73,6 @@ portfolioItems.forEach(item => {
 const getUrl = window.location.href;
 const getUrlPath = window.location.pathname.replace("/","");
 const getUrlParams = window.location.search.replace("?","");
-
 // View item button functionality
 const viewItemBtn = document.querySelector("button#viewItem");
 
@@ -88,70 +84,66 @@ if(viewItemBtn !== null && viewItemBtn !== undefined){
 
         viewPortfolioItem();
 
-        showItem.scrollIntoView(true); // Add smooth scrolling
+        showItem.scrollIntoView(true);// Add smooth scrolling
     });
 }
 
-function viewPortfolioItem() {
-    // Check if URL path leads to a portfolio item page
-    if (getUrlPath === "portfolio-item.html") {
+function viewPortfolioItem(){
+
+// Check if URL path leads to a portfolio item page
+    if(getUrlPath === "portfolio-item.html"){
         // Extract tag and URL from query parameters
-        const urlParams = getUrlParams.split("=");
-        const tagName = urlParams[0];
-        const url = tagName === "iframe" ? urlParams[1].split(";")[0] : urlParams[1];
-        const filePath = tagName === "iframe" ? urlParams[2] : url;
+        let slitParams = getUrlParams.split("=");
+        let tagName = slitParams[0];
+        let getUrl = (tagName === "iframe")? slitParams[1].split(";")[0] : slitParams[1];
+        let fpath = (tagName === "iframe")? slitParams[2] : getUrl;
 
         // Initialize variables
-        let name = "";
-        const itemViewer = document.querySelector(".site-view > .section-container");
-
+        let name = '';
+        let itemViewer = document.querySelector(".site-view > .section-container");
+        
         // Handle iframe scenario
-        if (tagName === "iframe") {
-        const iframe = document.createElement("iframe");
-        iframe.id = "itemViewer";
-        iframe.src = url;
-        iframe.frameborder = 0;
-        iframe.onload = () => {
-            iframe.style.opacity = 1;
-        };
-        iframe.loading = "lazy";
+        if(tagName === "iframe"){
+            let createIframe = document.createElement("iframe");
+            createIframe.id = "itemViewer";
+            createIframe.src = getUrl;
+            createIframe.setAttribute("frameborder", 0);
+            createIframe.setAttribute("onload", "this.style.opacity = 1");
+            createIframe.loading = "lazy";
+            
+            itemViewer.innerHTML = createIframe.outerHTML;
+            name = fpath.replace("-", " ").replace("_", " ");
 
-        itemViewer.innerHTML = iframe.outerHTML;
-        name = filePath.replace("-", " ").replace("_", " ");
         }
-
         // Handle image scenario
-        else if (tagName === "img") {
-        const img = document.createElement("img");
-        img.src = `./${tagName}/${url}`;
-        const fileName = url.split("/").pop().split(".")[0];
-        name = fileName.replace("-", " ").replace("_", " ");
-        if (img.alt.length === 0) {
-            img.alt = `${name} image`;
+        if(tagName === "img"){
+            let createImg = document.createElement("img");
+            createImg.src = `./${tagName}/${getUrl}`;
+            let fName = getUrl.split("/").pop().split(".")[0];
+            name = fName.replace("-", " ").replace("_", " ");
+            if(createImg.alt.length === 0){
+                createImg.alt = `${name} image`;
+            }
+            createImg.setAttribute("onload", "this.style.opacity = 1");
+    
+            itemViewer.innerHTML = createImg.outerHTML;
         }
-        img.onload = () => {
-            img.style.opacity = 1;
-        };
-
-        itemViewer.innerHTML = img.outerHTML;
-        }
-
+    
         // Update header image
-        const headerImg = document.querySelector(".section__image > img");
-        headerImg.src = `./img/${filePath}`;
+        headerImg = document.querySelector(".section__image > img");
+        headerImg.src = `./assets/img/${fpath}`;
         headerImg.alt = `${name} image`;
-        headerImg.style.borderRadius = "20px";
-
+        headerImg.setAttribute("style", "border-radius: 20px");
+        
         // Remove loading class after 3.5 seconds
         setTimeout(() => {
-        document.querySelector(".site-view .section-container").classList.remove("loading");
+            document.querySelector(".site-view .section-container").classList.remove("loading");
         }, 3500);
     }
 }
-  
+
 // Immediately invoke the function
 (function () {
     viewPortfolioItem();
-})();
-  
+}());
 
