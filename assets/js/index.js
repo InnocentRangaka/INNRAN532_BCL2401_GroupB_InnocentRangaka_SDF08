d3e3ced5079ec5bca2872314a58c521a89fa4c52
Innocent <156681388+InnocentRangaka@users.noreply.github.com>
2024-02-20 09:25:53 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -112,9 +112,9 @@ function viewPortfolioItem(){
             createIframe.setAttribute("frameborder", 0);
             createIframe.setAttribute("onload", "this.style.opacity = 1");
             createIframe.loading = "lazy";
+            createIframe.width = itemViewer.offsetWidth;
 
             if(itemViewer.offsetWidth >= 1){
-                createIframe.width = itemViewer.offsetWidth;
                 // let makeHeight = Math.floor(itemViewer.offsetWidth + 200); 
                 // createIframe.setAttribute("height", makeHeight);
             }
